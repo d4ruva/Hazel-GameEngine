78e85928d2d05681796335e3f14d32baebe2c628
D4ruva <dhruvadhangar@gmail.com>
2023-05-04 13:12:11 +0530

    Added Pre Compiled Headers

--- premake5.lua
+++ premake5.lua
@@ -31,6 +31,10 @@ project "Hazel"
 		"%{prj.name}/vendor/spdlog/include"
 	}
 
+	pchheader "hzpch.h"
+	pchsource "Hazel/src/hzpch.cpp"
+
+
 	filter "system:windows"
 		cppdialect "C++20"
 		staticruntime "On"
